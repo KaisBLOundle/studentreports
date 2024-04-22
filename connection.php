812d1f3bae96c31b7f4be65abdd99da00c7f2d03
Kais <buckleylarab.kh@oundleschool.org.uk>
2024-04-22 15:16:39 +0100

    commit

--- connection.php
+++ connection.php
@@ -17,7 +17,7 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-     
+    echo "Connected: " . $e->getMessage();
     }
 catch(PDOException $e)
     {
